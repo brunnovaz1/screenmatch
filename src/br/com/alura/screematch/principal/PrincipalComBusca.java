0d30d583722af7331973b69905e6ec0e64cf7cb4
brunnovaz1 <brunnovaz@gmail.com>
2025-04-03 16:33:20 -0300

    Exception

--- src/br/com/alura/screematch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screematch/principal/PrincipalComBusca.java
@@ -39,9 +39,16 @@ public class PrincipalComBusca {
                 .create();
         TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
         System.out.println(meuTituloOmdb);
-        Titulo meuTitulo = new Titulo(meuTituloOmdb);
-        System.out.println(" - - - - - - - - - - - - - ");
-        System.out.println(meuTitulo);
 
+        try {
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println(" - - - - - - - - - - - - - ");
+            System.out.println(meuTitulo);
+        } catch (NumberFormatException e){
+            System.out.println("Aconteceu um erro!");
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Programa finalizou normalmente!");
+        }
     }
 }
